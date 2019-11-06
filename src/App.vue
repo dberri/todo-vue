0291d07013467fa73480b2010daae1089a2e0596
David Berri <dwbwill@gmail.com>
2019-11-06 06:25:28 -0300

    Adds a todo container

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <ToDo />
+  </div>
 </template>
 
 <script>
+import ToDo from "./components/ToDo.vue";
+
 export default {
   name: "app",
-  components: {}
+  components: {
+    ToDo
+  }
 };
 </script>
 
@@ -16,18 +22,16 @@ body {
   padding: 0;
 }
 
-body {
-  height: 100vh;
-}
-
 #app {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   background: linear-gradient(145deg, rgba(65,184,131,1) 0%, rgba(53,73,94,1) 100%);
   color: #2c3e50;
+  display: flex;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
-  text-align: center;
-  height: 100%;
+  justify-content: center;
+  min-height: 100vh;
   overflow: hidden;
+  text-align: center;
 }
 </style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="ToDo">
+    <header class="ToDo-header"></header>
+    <main class="ToDo-main">
+      <ul class="ToDo-list"></ul>
+    </main>
+    <footer class="ToDo-footer">
+      <a href="#" class="ToDo-link is-active">All</a>
+      <a href="#" class="ToDo-link">Completed</a>
+      <a href="#" class="ToDo-link">Not completed</a>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDo",
+
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+.ToDo {
+  background-color: #fafafa;
+  border-radius: 5px;
+  border: 1px solid #364d5e;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 75px;
+  margin-top: 75px;
+  min-width: 400px;
+  width: 40%;
+}
+
+.ToDo-header {
+  padding: 0.5rem;
+}
+
+.ToDo-main {
+  padding: 0.5rem;
+  flex-grow: 1;
+}
+
+.ToDo-main .ToDo-list {
+  margin: 0;
+  padding: 0.25rem;
+}
+
+.ToDo-footer {
+  display: flex;
+  justify-content: space-around;
+  padding: 0.5rem;
+}
+
+.ToDo-footer .ToDo-link {
+  border-radius: 5px;
+  border: 1px solid transparent;
+  color: #2c3e50;
+  padding: 5px;
+  text-decoration: none;
+  width: 30%;
+}
+
+.ToDo-footer .ToDo-link:hover,
+.ToDo-footer .ToDo-link.is-active {
+  border-color: #2f4e60;
+}
+</style>
